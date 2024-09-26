5fd66c936d8b2820e25d9491f877542567717c11
Sietse Guijt <smguijt@outlook.com>
2024-09-26 02:34:12 +0200

    .

--- Sources/App/Modules/UserManagement/DTOs/UserManagementUserDTO.swift
+++ Sources/App/Modules/UserManagement/DTOs/UserManagementUserDTO.swift
@@ -4,13 +4,14 @@ import Vapor
 struct UserManagementUserDTO: Content {
     let Id: String?
     let caption: String?
+    let firstCharacter: String?
 
-    
-    
     init(ID: UUID?, 
          caption: String?) {
             
         self.Id = ID?.uuidString
         self.caption = caption
+        self.firstCharacter = self.caption?.first?.description
     }
+    
 }
